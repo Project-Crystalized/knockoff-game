9940af1fd3f20a17236b99442291be3afe7bde8d
ladycattv <ladyccattv@gmail.com>
2026-01-18 19:55:30 +0100

    added game_id to database

--- src/main/java/gg/knockoff/game/KnockoffDatabase.java
+++ src/main/java/gg/knockoff/game/KnockoffDatabase.java
@@ -19,12 +19,14 @@ public class KnockoffDatabase {
 
     public static void setup_databases() {
         String create_ko_games = "CREATE TABLE IF NOT EXISTS KnockoffGames ("
+                + "game_id INTEGER PRIMARY KEY,"
                 + "map STRING,"
                 + "winner_team STRING,"
                 + "gametype STRING,"
                 + "timestamp INTEGER"
                 + ");";
         String create_ko_players = "CREATE TABLE IF NOT EXISTS KoGamesPlayers ("
+                + "game INTEGER REFERENCES LiteStrikeGames(game_id),"
                 + "player_uuid BYTES,"
                 + "team STRING,"
                 + "kills INTEGER,"
@@ -35,7 +37,7 @@ public class KnockoffDatabase {
                 + "items_used INTEGER,"
                 + "games_won INTEGER"
                 + ");";
-
+        addGameIdColumn();
         try (Connection conn = DriverManager.getConnection(URL)) {
             Statement stmt = conn.createStatement();
             stmt.execute(create_ko_games);
@@ -44,6 +46,35 @@ public class KnockoffDatabase {
             Bukkit.getLogger().severe(e.getMessage());
             for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
                 Bukkit.getLogger().severe(ste.toString());
+            }
+        }
+    }
+
+    public static void addGameIdColumn(){
+        String create_id_column = "ALTER TABLE KnockoffGames ADD COLUMN game_id INTEGER PRIMARY KEY;";
+        String check_id_column = "SELECT game_id FROM KnockoffGames LIMIT 1;";
+
+        String create_game_column = "ALTER TABLE KoGamesPlayers ADD COLUMN game INTEGER REFERENCES KnockoffGames(game_id);";
+        String check_game_column = "SELECT game FROM KoGamesPlayers LIMIT 1;";
+
+        try (Connection conn = DriverManager.getConnection(URL)) {
+            conn.createStatement().execute(check_id_column);
+        } catch (SQLException e) {
+            // if we catch a sql error, it mean the column doesnt exist, so we add it
+            try (Connection conn = DriverManager.getConnection(URL)) {
+                conn.createStatement().execute(create_id_column);
+            } catch (SQLException ex) {
+                Bukkit.getLogger().severe("uh weird error, idk bro ;-; (id)");
+            }
+        }
+
+        try (Connection conn = DriverManager.getConnection(URL)) {
+            conn.createStatement().execute(check_game_column);
+        } catch (SQLException e) {
+            try (Connection conn = DriverManager.getConnection(URL)) {
+                conn.createStatement().execute(create_game_column);
+            } catch (SQLException ex) {
+                Bukkit.getLogger().severe("uh weird error, idk bro ;-; (game)");
             }
         }
     }
@@ -60,19 +91,21 @@ public class KnockoffDatabase {
             game_stmt.executeUpdate();
 
             String save_player = "INSERT INTO KoGamesPlayers(player_uuid, team, kills, deaths, blocks_placed, blocks_broken, items_collected, items_used, games_won)"
-                    + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement player_stmt = conn.prepareStatement(save_player);
             for (Player p : Bukkit.getOnlinePlayers()) {
                 PlayerData pd = gm.getPlayerData(p);
 
-                player_stmt.setBytes(1, uuid_to_bytes(p));
-                player_stmt.setString(2, Teams.GetPlayerTeam(p));
-                player_stmt.setInt(3, pd.kills);
-                player_stmt.setInt(4, pd.getDeaths());
-                player_stmt.setInt(5, pd.blocksplaced);
-                player_stmt.setInt(6, pd.blocksbroken);
-                player_stmt.setInt(7, pd.powerupscollected);
-                player_stmt.setInt(8, pd.powerupsused);
+                int game_id = conn.prepareStatement("SELECT last_insert_rowid();").executeQuery().getInt("last_insert_rowid()");
+                player_stmt.setInt(1, game_id);
+                player_stmt.setBytes(2, uuid_to_bytes(p));
+                player_stmt.setString(3, Teams.GetPlayerTeam(p));
+                player_stmt.setInt(4, pd.kills);
+                player_stmt.setInt(5, pd.getDeaths());
+                player_stmt.setInt(6, pd.blocksplaced);
+                player_stmt.setInt(7, pd.blocksbroken);
+                player_stmt.setInt(8, pd.powerupscollected);
+                player_stmt.setInt(9, pd.powerupsused);
                 if (WinningTeam.equals(Teams.GetPlayerTeam(p))) {
                     player_stmt.setInt(9, 1);
                 } else {

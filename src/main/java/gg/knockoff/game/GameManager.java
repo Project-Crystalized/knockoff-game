aacf8cd28a9a28fcb8a692e92f244130389a978e
MacoronieTheBonnonie <232444287+MacoronieTheBonnonie@users.noreply.github.com>
2026-09-25 02:20:40 +0100

    Simple fix to knock off spectators not getting the compass
    Simple fix to knock off spectators not getting the compass. More changes tommorow for spectators incoming, as there is quite a few bugs but I am very tired. Don't update knock off yet ton main server

--- src/main/java/gg/knockoff/game/GameManager.java
+++ src/main/java/gg/knockoff/game/GameManager.java
@@ -932,8 +932,6 @@ public class GameManager { //I honestly think this entire class could be optimis
             playerDatas.add(pd);
         }
         markEliminated(pd);
-
-        setSpectator(p);
         p.getInventory().clear();
         p.clearActivePotionEffects();
         p.teleport(knockoff.getInstance().mapdata.get_que_spawn(knockoff.getInstance().getGameWorld()));
@@ -942,6 +940,7 @@ public class GameManager { //I honestly think this entire class could be optimis
         p.setWorldBorder(PlayerBorder);
         PlayerBorder.setCenter(p.getX() + 0.5, p.getZ() + 0.5);
         PlayerBorder.setSize(3);
+        setSpectator(p);
 
         Bukkit.getLogger().info("[Knockoff] " + p.getName() + " joined mid-game as a spectator.");
     }
